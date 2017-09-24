9ba5bb8eef2cd0f25a4d9457ca6c2160c3695e60
Petr Rašek <bowman@hydratk.org>
2017-09-24 12:29:38 +0200

    Modified - FreeBSD platform supported.

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ st_setup(
     entry_points=entry_points,
     keywords='hydratk,data generation,json,xml,asn.1',
     requires_python='>=2.6,!=3.0.*,!=3.1.*,!=3.2.*',
-    platforms='Linux'
+    platforms='Linux,FreeBSD'
 )
 
 task.run_post_install(argv, config)
